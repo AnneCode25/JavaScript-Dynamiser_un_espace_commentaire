548355c4b5fc6ec9072948dcc490a798da74f916
AnneVi <villette55@gmail.com>
2024-04-16 20:26:28 +0200

    Fonction d'erreur si tous les champs ne sont pas remplis

--- app.js
+++ app.js
@@ -14,22 +14,28 @@ function addComment (){
     let lastName = document.getElementById("last-name").value;
     let comment = document.getElementById("message").value;
 
-    let newComment = document.createElement("div");
-    newComment.classList.add("flex", "space-x-4", "text-sm", "text-gray-500");
-    newComment.innerHTML = `
-        <div class="flex-1 py-10">
-            <h3 class="font-medium text-gray-900">${firstName + " " + lastName}</h3>
-            <div class="prose prose-sm mt-4 max-w-none text-gray-500">
-                <p>${comment}</p>
+    if (firstName === "" || lastName === "" || comment ===""){
+        document.getElementById("error-message").style.display="block";
+    }else {
+        let newComment = document.createElement("div");
+        newComment.classList.add("flex", "space-x-4", "text-sm", "text-gray-500");
+        newComment.innerHTML = `
+            <div class="flex-1 py-10">
+                <h3 class="font-medium text-gray-900">${firstName + " " + lastName}</h3>
+                <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                    <p>${comment}</p>
+                </div>
             </div>
-        </div>
-    `;
+        `;
+    
+        let commentList = document.getElementById("comment-list");
+    
+        commentList.appendChild(newComment);
+    
+        document.getElementById("first-name").value="";
+        document.getElementById("last-name").value="";
+        document.getElementById("message").value="";    
+        }
+    }
 
-    let commentList = document.getElementById("comment-list");
 
-    commentList.appendChild(newComment);
-
-    document.getElementById("first-name").value="";
-    document.getElementById("last-name").value="";
-    document.getElementById("message").value="";    
-}
