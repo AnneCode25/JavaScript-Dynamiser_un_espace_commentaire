a6de0e739abc6d0a8c5932d8f2c1d4728fd3ab83
AnneVi <villette55@gmail.com>
2024-04-16 20:15:51 +0200

    Fonction ajout commentaire

---
+++ app.js
@@ -0,0 +1,35 @@
+// Empêcher le rafraichissement de la page
+document.getElementById("bouton").addEventListener("click", function(event)
+{
+    //Cale empêche le rafraichissement de la page
+    event.preventDefault();
+
+
+    // appelle de la fonction qui ajoute un commentaire
+    addComment();
+});
+
+function addComment (){
+    let firstName = document.getElementById("first-name").value;
+    let lastName = document.getElementById("last-name").value;
+    let comment = document.getElementById("message").value;
+
+    let newComment = document.createElement("div");
+    newComment.classList.add("flex", "space-x-4", "text-sm", "text-gray-500");
+    newComment.innerHTML = `
+        <div class="flex-1 py-10">
+            <h3 class="font-medium text-gray-900">${firstName + " " + lastName}</h3>
+            <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                <p>${comment}</p>
+            </div>
+        </div>
+    `;
+
+    let commentList = document.getElementById("comment-list");
+
+    commentList.appendChild(newComment);
+
+    document.getElementById("first-name").value="";
+    document.getElementById("last-name").value="";
+    document.getElementById("message").value="";    
+}
